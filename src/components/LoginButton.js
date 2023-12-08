c4ee247efba0bd7a4691c4a663d17cff1475c851
Ernie_py <user@example.com>
2023-12-08 02:16:56 -0500

    D&L mode workgin with login bttn and link

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button } from 'react-bootstrap';
+import { DarkModeContext } from '../contexts/DarkModeContext';
+import { Link } from 'react-router-dom';
 
 function LoginButton() {
+    const { isDarkMode } = useContext(DarkModeContext);
+
     return (
-        <Button variant="outline-warning" className="btn-login position-fixed top-0 end-0 m-4">
+        <Button
+            as={Link}
+            to="/login"
+            variant={isDarkMode ? 'outline-warning' : 'outline-primary'}
+            className="btn-login position-fixed top-0 end-0 m-4"
+        >
             Login
         </Button>
     );
