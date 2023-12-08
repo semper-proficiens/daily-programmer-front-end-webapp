f420b1aedc8ba57843f51c0132fa3e2ae86c51bb
Ernie_py <user@example.com>
2023-12-08 02:24:14 -0500

    Bigger login button

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -6,12 +6,18 @@ import { Link } from 'react-router-dom';
 function LoginButton() {
     const { isDarkMode } = useContext(DarkModeContext);
 
+    const buttonStyle = {
+        fontSize: '1.5rem',
+        padding: '10px 20px'
+    };
+
     return (
         <Button
             as={Link}
             to="/login"
             variant={isDarkMode ? 'outline-warning' : 'outline-primary'}
             className="btn-login position-fixed top-0 end-0 m-4"
+            style={buttonStyle}
         >
             Login
         </Button>
